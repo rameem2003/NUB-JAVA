36e0f0d1ec444a708fd76fc353ddfe42fc2fae26
Mahmood Hassan Rameem <rameem2019@gmail.com>
2026-03-27 20:44:21 +0600

    practice

---
+++ PLAYGROUND/LoopContinue.java
@@ -0,0 +1,12 @@
+package PLAYGROUND;
+
+public class LoopContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue; // Skip the rest of the loop for even numbers
+            }
+            System.out.println(i); // This will only print odd numbers
+        }
+    }
+}
