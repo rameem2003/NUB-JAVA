bfe3b230aa3a84b7f77c80c59083b568bc3c8acb
Mahmood Hassan Rameem <rameem2019@gmail.com>
2026-03-27 20:23:57 +0600

    OK

---
+++ PLAYGROUND/Loop.java
@@ -0,0 +1,11 @@
+package PLAYGROUND;
+
+public class Loop {
+    public static void main(String[] args) {
+        int a, b;
+
+        for (a = 1, b = 4; a < b; a++, b--) {
+            System.out.println("a: " + a + " b: " + b);
+        }
+    }
+}
